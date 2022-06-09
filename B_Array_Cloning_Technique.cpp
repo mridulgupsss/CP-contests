cd45789e5c03dbdd9809c27693b349be759fa66a
Mridul <gmridul556@gmail.com>
2022-06-09 18:35:07 +0530

    cp submissions

--- B_Array_Cloning_Technique.cpp
+++ B_Array_Cloning_Technique.cpp
@@ -5,109 +5,93 @@
 #include <string>
 #define int long long
 #define NFS ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
- 
 #define all(x) (x).begin(), (x).end() //Forward traversal
- 
-
-
-
-
-// loops 
+#define rall(x) (x).rbegin(), (x).rend() //Backward traversal
+#define YES cout<<"YES"<<endl;
+#define NO cout<<"NO"<<endl;
 #define f(i, n) for (int i = 0; i < n; i++)
 #define f1(i, n) for (int i = 1; i <= n; i++)
 #define loop(i,a,b) for (int i = a; i < b; i++)
 #define bloop(i,a,b) for (int i = a ; i>=b;i--)
- 
-// vector
 #define vi vector<int>
+#define vpii vector<pair<int, int>>
+#define vvi vector<vector<int>>
 #define pb push_back
 #define fi first
-#define se second
- 
+#define se second 
 #define endl "\n"
- 
 #define max3(a, b, c) max(max((a), (b)), (c))
 #define max4(a, b, c, d) max(max((a), (b)), max((c), (d)))
 #define min3(a, b, c) min(min((a), (b)), (c))
 #define min4(a, b, c, d) min(min((a), (b)), min((c), (d)))
-
- 
 using namespace std;
- 
 const int mod = 1e9 + 7;
-const int inf = 1e17;
+const int inf = 1e18;
+#define debug(v)          \
+    for (auto i : v)      \
+    {                     \
+        cout << i << " "; \
+    }                     \
+    cout << endl;
+#define debugmp(v)                                  \
+    for (auto it : v)                                \
+    {                                               \
+        cout << it.first << " " << it.second << endl; \
+    }
+
+// count digits
+//  int countDigit(int n)
+// {
+//    if(n == 0)
+//      return 1;
+//     int count = 0;
+//     while (n != 0)
+//     {
+//         n = n / 10;
+//         ++count;
+//     }
+//     return count;
+// }
  
- 
-//  // maths
+// // maths
 // int mod_add(int a, int b) {a = a % mod; b = b % mod; return (((a + b) % mod) + mod) % mod;}
 // int mod_mul(int a, int b) {a = a % mod; b = b % mod; return (((a * b) % mod) + mod) % mod;}
 // int inv(int i) { if (i == 1) return 1; return (mod - ((mod / i) * inv(mod % i)) % mod) % mod;}
 // int pwr(int a, int b) {a %= mod; int res = 1; while (b > 0) {if (b & 1) res = res * a % mod; a = a * a % mod; b >>= 1;} return res;}
 // int gcd(int a, int b) { if (b == 0) return a; return gcd(b, a % b);}
- 
+
 // // string palindrome 
 // int isPalindrome(string S){string P = S; reverse(P.begin(), P.end()); if (S == P) return 1; else return 0; }
-
 
 // compare function
 // bool sortbysec(pair<long long int, long long int> &a, pair<long long int, long long int> &b) { return (a.second < b.second); } 
 // bool cmp(pair<long long int a, pair<long long int b){if (a.first > b.first)return true;else if (a.first == b.first){if (a.second > b.second)return true;}return false;}
- 
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+         
+
+// Actual Code Here : 
 signed main(){
- 
+
+
     int t;
-    cin>>t;
+    cin >> t;
     while(t--){
-        int mn = INT_MAX, mx =INT_MIN;
-       int n;
-       cin>>n;
-        vector<int> v;
-        unordered_map<int, int> mp;
-        int max_f=-1, max_a;
-        for(int i=0; i<n; i++){
-            int a;
-            cin>>a;
-            v.pb(a); 
-            mp[a]++;
-            if(mp[a]>max_f){
-                max_f=mp[a];
-                max_a=a;
-            }
-        }
-        int f=max_f;
-int ans=0;
-        if(max_f==n) ans=0;
-        else{
+       int n; cin>>n; 
+       vector<int> v;
+       for(int i=0; i<n; i++){
+           int a; cin>>a;
+           v.pb(a);
+           
+       }
        
-        n=n-f;
-        ans++;
-     
-
-            while(1){
-                if(f<n){
-                     n=n-f;
-                    ans+=f;
-                    f=f*2;
-                    ans++;
-                   
-                }
-                else{
-                    ans+=n;
-                    break;
-                }
-            }
-
-            
-        }
-
-        cout<<ans<<endl;
-        
 
 
-
-
-    }
+  }
 }
-
 
 
