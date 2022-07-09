2244609e0871c7290061f83439e48f6b3b853780
Mridul <gmridul556@gmail.com>
2022-07-09 12:04:46 +0530

    updated

---
+++ B_2_Social_Network_hard_version.cpp
@@ -0,0 +1,129 @@
+/* Mridul Gupta (ambarsariya_mg) */
+ 
+#include "bits/stdc++.h"
+#include <string>
+#define int long long
+#define NFS ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end() //Forward traversal
+#define rall(x) (x).rbegin(), (x).rend() //Backward traversal
+#define YES cout<<"YES"<<endl;
+#define NO cout<<"NO"<<endl;
+#define f(i, n) for (int i = 0; i < n; i++)
+#define f1(i, n) for (int i = 1; i <= n; i++)
+#define loop(i,a,b) for (int i = a; i < b; i++)
+#define bloop(i,a,b) for (int i = a ; i>=b;i--)
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vpii vector<pair<int, int>>
+#define pii pair<int, int>
+#define pb push_back
+#define fi first
+#define se second 
+#define endl "\n"
+#define lcm(a, b) int lcm(int a, int b) { return (a / __gcd(a, b)) * b; }
+#define setbits(x) __builtin_popcountll(x)
+#define debug(a) cout<<a<<endl;
+#define debug2(a, b) cout<<a<<" "<<b<<endl;
+#define debug3(a, b, c) cout<<a<<" "<<b<<" "<<c<<endl; 
+#define debug4(a, b, c, d) cout<<a<<" "<<b<<" "<<c<<" "<<d<<endl;  
+#define debug5(a, b, c, d, e) cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e<<endl;  
+#define debugarr(v) for(auto i : v){cout<<i<<" ";}cout<<endl;
+#define debugmp(v) for(auto it:v){cout<<it.first<<" "<<it.second<<endl;}
+using namespace std;
+const int mod = 1e9 + 7;
+const int inf = 1e18;
+// count digits
+//  int countDigit(int n)
+// {
+//    if(n == 0)
+//      return 1;
+//     int count = 0;
+//     while (n != 0)
+//     {
+//         n = n / 10;
+//         ++count;
+//     }
+//     return count;
+// }
+ 
+// // maths
+// int mod_add(int a, int b) {a = a % mod; b = b % mod; return (((a + b) % mod) + mod) % mod;}
+// int mod_mul(int a, int b) {a = a % mod; b = b % mod; return (((a * b) % mod) + mod) % mod;}
+// int inv(int i) { if (i == 1) return 1; return (mod - ((mod / i) * inv(mod % i)) % mod) % mod;}
+// int pwr(int a, int b) {a %= mod; int res = 1; while (b > 0) {if (b & 1) res = res * a % mod; a = a * a % mod; b >>= 1;} return res;}
+// int gcd(int a, int b) { if (b == 0) return a; return gcd(b, a % b);}
+
+// // string palindrome 
+// int isPalindrome(string S){string P = S; reverse(P.begin(), P.end()); if (S == P) return 1; else return 0; }
+
+// compare function
+// bool sortbysec(pair<long long int, long long int> &a, pair<long long int, long long int> &b) { return (a.second < b.second); } 
+// bool cmp(pair<long long int a, pair<long long int b){if (a.first > b.first)return true;else if (a.first == b.first){if (a.second > b.second)return true;}return false;}
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+         
+
+// Actual Code Here : 
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+ 
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+gp_hash_table < int ,bool , custom_hash > gp;
+signed main(){
+    queue < int > bes ;
+    int n , k  , ans; cin >> n >> k ;
+    for(int i = 0 ; i< n ; i++){
+            cin >> ans ;
+            if(gp[ans])
+                    continue;
+            if(int(bes.size()) < k )
+            {
+                bes.push(ans);
+                gp[ans] = 1;
+            }
+            else{
+                int v = bes.front();
+                gp[v] = 0 ;
+                bes.pop();
+                gp[ans] = 1;
+                bes.push(ans);
+            }
+    }
+    vector < int >res = { };
+    while(!bes.empty()){
+            int u = bes.front();
+            bes.pop();
+            res.push_back(u);
+    }
+    reverse(res.begin() , res.end());
+    cout << res.size() << endl ;
+    for(int i = 0 ; i< res.size(); i++)
+                cout << res[i] << " " ;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
